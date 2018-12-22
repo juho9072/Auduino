bc5c42bef85399028e72e2325b247c69f413b19c
juho9072 <juho9072@gmail.com>
2018-12-22 23:55:00 +0900

    초음파센서 설명 추가

--- Ultra_Sound_Example/Ultra_Sound_Example.ino
+++ Ultra_Sound_Example/Ultra_Sound_Example.ino
@@ -1,8 +1,9 @@
-int e = 12;
-int t = 13;
+//초음파센서의 핀번호 설정
+int e = 12; // 12번핀 = echo
+int t = 13; // 13번핀 = trig
 
-void setup() {
-
+void setup() 
+//t 를 출력하게 하고 e를 입력받게 한다.
 Serial.begin(9600);
 pinMode(t,OUTPUT);
 pinMode(e,INPUT);
@@ -12,14 +13,18 @@ pinMode(e,INPUT);
 void loop() {
 float duration, distance;
 
-  
+//t에서 초음파를 보낸다.
 digitalWrite(t,HIGH);
 delay(10);
+//그 뒤에는 대기상태로 한다.
 digitalWrite(t,LOW);
 
+e가 출력한 시간을 저장한다.
 duration = pulseIn(e, HIGH);
+//초음파가 나갔다가 돌아온 시간을 계산해서 거리를 표시한다.
 distance = ((float)(340 * duration) / 1000) / 2; 
 
+//나온 값을 출력한다.
 Serial.print(distance);
   Serial.println("mm");
   delay(10);
