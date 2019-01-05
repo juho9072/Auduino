f373b385d239fc793776cf2c1b2c37f4fd4b2459
juho9072 <juho9072@gmail.com>
2019-01-05 18:09:33 +0900

    쓰레기통 작동 부연설명 추가, 작동시 딜레이 감소

--- trash/trash.ino
+++ trash/trash.ino
@@ -1,28 +1,28 @@
 Servo trash: //서브모터 객체 설정(이름 : trash)
 
-int t = 8:
-int e = 9:
+int t = 8:        //(t = trig)
+int e = 9:      //(e =     )
 
 void setup() {
   trash.attach(10):
-  pinMode(t,OUTPUT):
-  pinmode(e,INPUT):
+  pinMode(t,OUTPUT):     //t에서 출력
+  pinMode(e,INPUT):    //e에서 입력
 
 }
 
 void loop() {
-digitalWrite(t,HIGH):  //초다림
-digitalWrite(t,LOW):  // 초음파 멈추기
+digitalWrite(t,HIGH):    //초음파 보내기
+digitalWrite(t,LOW):   // 초음파 멈추기
    
 long duration = pulseln(e,HIGH):
-//초음파가 나갔다 온 시간을 측정해 
+                           //초음파가 나갔다 온 시간을 측정한다
 long distance = (340 * duration/2/10000:
-    //거리를 계산해 distance에 저장
+                        //거리를 계산해 distance에 저장 
     
-if (distance <= 10) {   //만약 초음파센서와 물체의 거리가 10cm 이하라면
-  trash.write(100):   // 서브모터의 각도를 100도로 조정
-  delay(3000):       // 3초 대기
-}else{                         //초음파와 물체의 거리가 10cm 보다 크다면
-  trash.write(10):           // 서브모터의 각도를 0도로 조정
+if (distance <= 10) {      //만약 초음파센서와 물체의 거리가 10cm 이하라면
+  trash.write(80):       // 서브모터의 각도를 80도로 변경 = 쓰레기통 뚜껑 열림
+  delay(100):          // 0.1초 대기(1초 = 1000)
+}else{                            //초음파센서와 물체의 거리가 10cm 초과라면
+  trash.write(0):               // 서브모터의 각도를 0도로 조정 = 쓰레기통 뚜껑 닫힘
     }
 }
