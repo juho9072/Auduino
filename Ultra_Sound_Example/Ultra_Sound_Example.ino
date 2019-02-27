4e6decb1e07ec02318c1c44be1f21bb832813ae4
juho9072 <juho9072@gmail.com>
2019-02-27 21:14:10 +0900

    쓰레기

--- Ultra_Sound_Example/Ultra_Sound_Example.ino
+++ Ultra_Sound_Example/Ultra_Sound_Example.ino
@@ -1,10 +1,9 @@
 //초음파센서의 핀번호 설정
-int e = 12; // 12번핀 = echo
-int t = 13; // 13번핀 = trig
+int e = 8; // 12번핀 = echo
+int t = 9; // 13번핀 = trig
 
 void setup() 
 //t 를 출력하게 하고 e를 입력받게 한다.
-Serial.begin(9600);
 pinMode(t,OUTPUT);
 pinMode(e,INPUT);
 }
@@ -19,7 +18,7 @@ delay(10);
 //그 뒤에는 대기상태로 한다.
 digitalWrite(t,LOW);
 
-e가 출력한 시간을 저장한다.
+// e가 출력한 시간을 저장한다.
 duration = pulseIn(e, HIGH);
 //초음파가 나갔다가 돌아온 시간을 계산해서 거리를 표시한다.
 distance = ((float)(340 * duration) / 1000) / 2; 
--- trash/trash.ino
+++ trash/trash.ino
@@ -1,28 +1,35 @@
-Servo trash: //서브모터 객체 설정(이름 : trash)
+#include   <Servo.h>
 
-int t = 8:        //(t = trig)
-int e = 9:      //(e =     )
+Servo trash;     //서브모터 객체 설정(이름 : trash)
+
+int trig = 8;        //(t = trig)
+int echo = 9;      //(e =     )
 
 void setup() {
-  trash.attach(10):
-  pinMode(t,OUTPUT):     //t에서 출력
-  pinMode(e,INPUT):    //e에서 입력
-
-}
+  Serial.begin(9600);
+ trash.attach(10);
+    pinMode(trig, OUTPUT);
+    pinMode(echo, INPUT);
+    }
+    
 
 void loop() {
-digitalWrite(t,HIGH):    //초음파 보내기
-digitalWrite(t,LOW):   // 초음파 멈추기
-   
-long duration = pulseln(e,HIGH):
+digitalWrite(trig,HIGH);
+delayMicroseconds(10);
+digitalWrite(trig,LOW);
+
+
+long duration = pulseIn(echo,HIGH);
                            //초음파가 나갔다 온 시간을 측정한다
-long distance = (340 * duration/2/10000:
+long distance= (340 * duration)/2/10000;
                         //거리를 계산해 distance에 저장 
-    
+Serial.println(distance);
+
 if (distance <= 10) {      //만약 초음파센서와 물체의 거리가 10cm 이하라면
-  trash.write(80):       // 서브모터의 각도를 80도로 변경 = 쓰레기통 뚜껑 열림
-  delay(100):          // 0.1초 대기(1초 = 1000)
+  trash.write(0);     // 서브모터의 각도를 80도로 변경 = 쓰레기통 뚜껑 열림
+  delay(100);    // 0.1초 대기(1초 = 1000)
 }else{                            //초음파센서와 물체의 거리가 10cm 초과라면
-  trash.write(0):               // 서브모터의 각도를 0도로 조정 = 쓰레기통 뚜껑 닫힘
+  trash.write(80);
+  // 서브모터의 각도를 0도로 조정 = 쓰레기통 뚜껑 닫힘
     }
 }
